eec68e96fec1a90174b0454b51b029a8851ef46a
vact19 <chj6703@gmail.com>
2023-02-07 18:52:07 +0900

    init

---
+++ src/main/java/xyz/hugme/hugmebackend/HugmebackendApplication.java
@@ -0,0 +1,13 @@
+package xyz.hugme.hugmebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HugmebackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HugmebackendApplication.class, args);
+    }
+
+}
