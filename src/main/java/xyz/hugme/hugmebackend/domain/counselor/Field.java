550ad3190dbfde3266fad6f8f1a9ee23529b6e9a
vact19 <chj6703@gmail.com>
2023-02-11 23:52:37 +0900

    feat: Field.java Description

--- src/main/java/xyz/hugme/hugmebackend/domain/counselor/Field.java
+++ src/main/java/xyz/hugme/hugmebackend/domain/counselor/Field.java
@@ -2,11 +2,21 @@ package xyz.hugme.hugmebackend.domain.counselor;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
 
+@Getter
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
-public enum Field {
+public enum Field { // 상담사 전문분야
 
-    ELDER, DEPRESSION, RELATIONSHIP, YOUTH;
+    ELDER("노인"), DEPRESSION("우울"),
+    RELATIONSHIP("인간관계"), YOUTH("청소년"),
+    ANXIETY("불안");
+
+    private final String desc;
+
+    Field(String desc) {
+        this.desc = desc;
+    }
 
     // 역직렬화를 위함. FieldConverter에서 사용.
     @JsonCreator
