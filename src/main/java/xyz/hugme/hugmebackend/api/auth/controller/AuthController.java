6a4e33d0182f39bc972e5fd6c8afc44e82e568ab
vact19 <chj6703@gmail.com>
2023-02-23 13:44:33 +0900

    feat: enable Cross-Origin Cookie

--- src/main/java/xyz/hugme/hugmebackend/api/auth/controller/AuthController.java
+++ src/main/java/xyz/hugme/hugmebackend/api/auth/controller/AuthController.java
@@ -1,7 +1,6 @@
 package xyz.hugme.hugmebackend.api.auth.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +8,7 @@ import xyz.hugme.hugmebackend.api.auth.dto.LoginDto;
 import xyz.hugme.hugmebackend.api.counselor.service.ApiCounselorService;
 import xyz.hugme.hugmebackend.domain.user.counselor.Counselor;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
@@ -22,7 +19,7 @@ public class AuthController {
     // 상담사 로그인
     @PostMapping("/sign-in/counselor")
     public String signIn(@RequestBody LoginDto.Request requestDto, HttpServletRequest request
-                                        , HttpServletResponse response){
+                                      ){
         // username, password 검사, 토큰 생성 후 반환
         Counselor validatedCounselor = apiCounselorService.signIn(requestDto);
 
@@ -30,31 +27,37 @@ public class AuthController {
         HttpSession session = request.getSession();
         session.setAttribute("name", validatedCounselor.getName());
         session.setAttribute("email", validatedCounselor.getEmail());
-        Cookie cookie = new Cookie("cookie's-email", validatedCounselor.getEmail());
-        cookie.setPath("/");
-        response.addCookie(cookie);
-        Cookie cookie2 = new Cookie("cookie's-contact", validatedCounselor.getContact());
-        cookie2.setPath("/sdfsdfdsf");
-        response.addCookie(cookie2);
-        return "ok";
+
+        return "success";
     }
 
-    @GetMapping("/sign-in-test")
-    public String test(HttpServletRequest request){
-        StringBuilder sb = new StringBuilder();
-        String name = (String) request.getSession().getAttribute("name");
-        sb.append("session: name " + name);
-        String email = (String) request.getSession().getAttribute("email");
-        sb.append("session: email " + email);
-        Cookie[] cookies = request.getCookies();
-        sb.append("수동등록 쿠키값 출력 시작 : ");
-        for (Cookie cookie : cookies) {
-            sb.append("쿠키 이름 : " + cookie.getName());
-            sb.append("쿠키 값 : " + cookie.getValue());
-        }
-
-        return sb.toString();
-    }
+    /** 아래 주석은 실 배포환경에서 CORS 문제가 발생할 시 활용해보자.*/
+//    @GetMapping("/sign-in-test")
+//    public String test(HttpServletRequest request){
+//        StringBuilder sb = new StringBuilder();
+//        HttpSession session = request.getSession();
+//
+//        String name = (String) session.getAttribute("name");
+//        System.out.println("name = " + name);
+//        sb.append("session: name " + name);
+//        String email = (String) session.getAttribute("email");
+//        System.out.println("email = " + email);
+//        sb.append("session: email " + email);
+//
+//        return sb.toString();
+//    }
+//
+//    @GetMapping("/auth-header-test")
+//    public String authHeaderTest(HttpServletRequest request){
+//        String header = request.getHeader(HttpHeaders.AUTHORIZATION);
+//        System.out.println("header = " + header);
+//
+//        HttpSession session = request.getSession();
+//
+//        String name = (String) session.getAttribute("name");
+//        System.out.println("name = " + name);
+//        return header+ "그리고 세션name은" + name;
+//    }
 
 
 }
--- src/main/java/xyz/hugme/hugmebackend/global/config/WebConfig.java
+++ src/main/java/xyz/hugme/hugmebackend/global/config/WebConfig.java
@@ -2,6 +2,7 @@ package xyz.hugme.hugmebackend.global.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import xyz.hugme.hugmebackend.global.converter.FieldConverter;
@@ -14,10 +15,17 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("*")
-//                .allowCredentials(true)
-//                .maxAge(3000) // maxage 만큼 preflight 캐싱
+                .allowedOriginPatterns("*") // Request Header의 Origin을, Response Header의 Access-Control-Allow-Origin에 그대로 넣어준다.
+                .allowedMethods(HttpMethod.GET.name()
+                        ,HttpMethod.POST.name()
+                        ,HttpMethod.PATCH.name()
+                        ,HttpMethod.PUT.name()
+                        ,HttpMethod.DELETE.name()
+                )
+                .allowCredentials(true)
+                .maxAge(1800)
+                // maxage 만큼 preflight 캐싱은 기본 1800sec(30m)
+                // Access-Control-Max-Age=1800
         ;
     }
 
@@ -26,4 +34,6 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addConverter(new FieldConverter());
         registry.addConverter(new GenderConverter());
     }
+
+
 }
