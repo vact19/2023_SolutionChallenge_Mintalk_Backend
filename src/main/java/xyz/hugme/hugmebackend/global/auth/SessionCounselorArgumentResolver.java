1196bdf15ac13fb270cda947cc8ef844f77ae1e9
vact19 <chj6703@gmail.com>
2023-02-24 01:19:26 +0900

    comment: HttpSession LifeCycle

--- src/main/java/xyz/hugme/hugmebackend/global/auth/SessionCounselorArgumentResolver.java
+++ src/main/java/xyz/hugme/hugmebackend/global/auth/SessionCounselorArgumentResolver.java
@@ -15,7 +15,7 @@ import javax.servlet.http.HttpSession;
 @RequiredArgsConstructor
 @Component
 public class SessionCounselorArgumentResolver implements HandlerMethodArgumentResolver {
-    private final HttpSession session; // Yes, if you inject an HttpSession object via the Spring Boot framework, it will be the same HttpSession object that is associated with the request's JSESSIONID cookie. The Spring Boot framework will use this JSESSIONID cookie to retrieve the associated HttpSession object and inject it into your application
+    private final HttpSession session; // Yes, if you inject an HttpSession object via the Spring Boot framework, it will be the same HttpSession object that is associated with the request's JSESSIONID cookie. The Spring Boot framework will use this JSESSIONID cookie to retrieve the associated HttpSession object and inject it into your application. // HttpSession의 Bean LifeCycle은? => No, an HttpSession object is not a singleton bean. The lifecycle of an HttpSession bean is determined by the container. Generally, an HttpSession will remain active until the user's session expires, or until the session is invalidated. The session can also be explicitly invalidated by the application, which will cause the HttpSession to be destroyed.
     private final CounselorService counselorService;
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
