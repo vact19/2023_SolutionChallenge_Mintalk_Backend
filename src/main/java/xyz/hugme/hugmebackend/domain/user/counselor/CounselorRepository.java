25fd48834b1d4529655e96d3c5bf2bbcb942c620
vact19 <chj6703@gmail.com>
2023-02-23 22:14:37 +0900

    fix: NPE 방지 LEFT JOIN

--- src/main/java/xyz/hugme/hugmebackend/domain/user/counselor/CounselorRepository.java
+++ src/main/java/xyz/hugme/hugmebackend/domain/user/counselor/CounselorRepository.java
@@ -4,10 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
+
 public interface CounselorRepository extends JpaRepository<Counselor, Long> {
 
-    @Query("SELECT distinct c FROM Counselor c JOIN FETCH c.counselorReviews cr where c.id = :id")
-    Counselor findByIdFetchReviews(@Param("id") Long id);
+    // Review 가 Null 일 경우를 대비해, LEFT JOIN 한다. 모든 교집합과 A 차집합 (A - B)을 조회
+    @Query("SELECT distinct c FROM Counselor c LEFT JOIN FETCH c.counselorReviews cr where c.id = :id")
+    Optional<Counselor> findByIdFetchReviews(@Param("id") Long id);
 
 //    @Query("SELECT c.password FROM Counselor c WHERE c.email = :email")
 //    String findPasswordByEmail(@Param("email") String email);
