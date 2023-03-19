50b0fc2688a0f44889ad4183e6813ca8736bd3b3
vact19 <chj6703@gmail.com>
2023-03-19 17:40:19 +0900

    comment: SessionStatus.java

--- src/main/java/xyz/hugme/hugmebackend/global/auth/SessionStatus.java
+++ src/main/java/xyz/hugme/hugmebackend/global/auth/SessionStatus.java
@@ -5,7 +5,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-// 사용자의 로그인정보를 파악하기 위한 ArgumentResolver
+// 사용자의 로그인정보를 파악 && 쿠키 재발급하기 위한 ArgumentResolver
 
 // Handler Method 에
 /**@SessionStatus UserStatus userStatus 이렇게 사용*/
